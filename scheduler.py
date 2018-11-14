e41b1978e32940b0db681975500f3e8321493aef
smashed-pumpkin <33178925+smashed-pumpkin@users.noreply.github.com>
2018-11-14 22:48:41 +0100

    Delete scheduler.py

--- scheduler.py
+++
@@ -1,8 +0,0 @@
-from crontab import CronTab
-
-#cron = CronTab(user='spashankova')  
-cron = CronTab(tabfile='filename.tab')
-job = cron.new(command='C:/Users/spashankova/BicingPy/tracking+availability.py')  
-job.minute.every(5)
-
-cron.write()  
